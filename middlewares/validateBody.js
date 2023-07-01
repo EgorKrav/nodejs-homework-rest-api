931025c6c6783a5a8861bc789d00b4f5f998da0c
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-07-01 18:14:52 +0300

    hw02-fix3

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,15 +2,25 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-    if (Object.keys(req.body).length === 0) {
-      next(HttpError(400, "missing fields"));
-    } else {
+    if (req.method === "PUT") {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ message: "missing fields" });
+      } else {
+        const { error } = schema.validate(req.body);
+        if (error) {
+          return res.status(400).json({ message: error.message });
+        }
+        next();
+      }
+    } else if (req.method === "POST") {
       const { error } = schema.validate(req.body);
       if (error) {
-        next(HttpError(400, error.message));
+        return res.status(400).json({ message: error.message });
       }
       next();
-    } 
+    } else {
+      next();
+    }
   };
 
   return func;
